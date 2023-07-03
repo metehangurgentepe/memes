d479e9346e5176105aec439f141b96e9d13f63c7
metehangurgentepe <metehangurgentepe@gmail.com>
2023-07-03 12:02:02 +0300

    Initial Commit

---
+++ memesSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  memesSwiftUI
+//
+//  Created by Metehan Gürgentepe on 3.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ memesSwiftUI/memesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  memesSwiftUIApp.swift
+//  memesSwiftUI
+//
+//  Created by Metehan Gürgentepe on 3.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct memesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
